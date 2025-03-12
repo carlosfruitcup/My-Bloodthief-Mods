49477585ec36c77de3de2319c339d92d5f5063b5
carlosfruitcup <40542088+carlosfruitcup@users.noreply.github.com>
2025-03-12 18:26:30 -0400

    Fixed BatPOV to actually be BatPOV
    Fixed BatPOV to actually be BatPOV
    Was testing out someone else's code

--- carlosfruitcup-BatPOV/mod_main.gd
+++ carlosfruitcup-BatPOV/mod_main.gd
@@ -1,86 +1,52 @@
 extends "res://addons/ModLoader/mod_node.gd"
 
-var checkpoint_data = {}
-var game_paused = false
-
+var theBat : Node3D
+var toggle : bool = false
 
 func init():
-	ModLoader.mod_log(name_pretty + " Checkpoint Mod loaded!")
+	ModLoader.mod_log("Bat POV loaded")
+	
+	add_input_event("toggleBatPOV",[KEY_TAB])
+	
+	#Connects to the level loaded signal, which only activates once.
+	Signals.level_loaded.connect(look4Bat)
 
-	add_input_event("checkpoint_create", [KEY_Q])
-	add_input_event("checkpoint_load", [KEY_E])
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("toggleBatPOV"):
+		toggle = !toggle
 
-
-func _process(_delta):
-	if Input.is_action_just_pressed("checkpoint_create"):
-		create_checkpoint()
-
-	elif Input.is_action_just_pressed("checkpoint_load"):
-		load_checkpoint()
-
-	if game_paused and has_input():
-		resume_game()
-
-
-func create_checkpoint():
-	var player = GameManager.get_player()
-	if not player:
-		print("Player not Valid")
-		return
-
-	checkpoint_data = {
-		"global_position": player.global_position,
-		"velocity_y": player.velocity_y,
-		"velocity": player.velocity,
-		"accel": player.accel,
-		"blood_amount": player.blood_amount,
-		"player_camera": GameManager.get_player_camera(),
-		"tick_count": GameManager.tick_count
-	}
-
-	ModLoader.mod_log("Checkpoint saved.")
+func look4Bat() -> void:
+	theBat = null
+	add_input_event("toggleBatPOV",[KEY_TAB])
+	find(get_tree().current_scene)
 
 
-func load_checkpoint():
-	var player = GameManager.get_player()
-	if not player or checkpoint_data.is_empty():
-		print("Player not Valid")
-		return
-
-	player.ground_pound_component.disable()
-
-	var checkpoint_variables = {
-		"global_position": player.global_position,
-		"velocity_y": player.velocity_y,
-		"velocity": player.velocity,
-		"accel": player.accel,
-		"blood_amount": player.blood_amount,
-		"direction": GameManager.player_camera,
-		"tick_count": GameManager.tick_count,
-	}
-
-	for checkpoint_value in checkpoint_variables:
-		var game_variable = checkpoint_variables[checkpoint_value]
-		game_variable = checkpoint_data[checkpoint_value]
-		print(game_variable)
-		print(checkpoint_data[checkpoint_value])
-		print(checkpoint_value)
-
-	game_paused = true
-	get_tree().paused = true
-
-	ModLoader.mod_log("Checkpoint loaded. Press any key to resume.")
+func createCamera() -> void:
+	var cam = Camera3D.new()
+	cam.fov = 120
+	cam.physics_interpolation_mode = true
+	theBat.add_child(cam)
+	cam.current = true
 
 
-func resume_game():
-	game_paused = false
-	get_tree().paused = false
+func find(parent):
+	for i in parent.get_children():
+		print(i.name)
+		if i.name == "bat_combined_4_0":
+			theBat = i.get_parent().get_parent()
+			createCamera()
+			print("FOUND")
+			break
 
-	ModLoader.mod_log("Game resumed.")
+		find(i)
 
 
-func has_input() -> bool:
-	for action in InputMap.get_actions():
-		if Input.is_action_pressed(action):
-			return true
-	return false
+
+func _process(_delta: float) -> void:
+	if is_instance_valid(theBat):
+		if toggle:
+			theBat.get_node("Camera3D").current = true 
+			GameManager.player.camera.current = false
+		else:
+			theBat.get_node("Camera3D").current = false 
+			GameManager.player.camera.current = true
